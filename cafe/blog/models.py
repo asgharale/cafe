8eb6fc44a37bf2b6e67199773212c48d68d4455a
asgharale <asgharale11@gmail.com>
2024-09-05 04:31:49 +0330

    user app

--- cafe/blog/models.py
+++ cafe/blog/models.py
@@ -1,8 +1,23 @@
 from django.db import models
 
 class Article(models.Model):
+    STATUSES = (
+        ('T', "Published"),
+        ('F', "Draft"),
+    )
+
+    id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=255)
     address = models.SlugField(max_length=255, unique=True)
     meta = models.TextField(max_length=400)
-    # tag, category, body, thumbnail, some ai features
+    thumbnail = models.ImageField(upload_to="blog/thumbnails_%Y/", blank=True, max_length=255)
+    # tag, category, body(reach text editor), some ai features, comments 
     author = models.ForeignKey("user.CUser", on_delete=models.CASCADE)
+    status = models.CharField(max_length=1, choices=STATUSES)
+    read_time = models.SmallIntegerField()
+    views = models.IntegerField()
+    likes = models.IntegerField()
+    created_at = models.DateField()
+    last_modified = models.DateField(auto_now=True)
+    
+    open_conv = models.BooleanField(default=True)
--- cafe/user/models.py
+++ cafe/user/models.py
@@ -1,10 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
-from django.contrib.auth.models import AbstractUser
 
-# custom
-class CUser(AbstractUser):
+class CUserManager(BaseUserManager):
+    def create_user(self, email, username, first_name, last_name, password=None):
+        if not email:
+            raise ValueError("Users must have an email address")
+
+        user = self.model(
+            email=self.normalize_email(email),
+            username=username,
+            first_name=first_name,
+            last_name=last_name
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, first_name, last_name, password=None):
+        user = self.create_user(
+            email,
+            password=password,
+            username=username,
+            first_name=first_name,
+            last_name=last_name
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+class CUser(AbstractBaseUser):
 	email = models.EmailField(unique=True, max_length=255)
+	username = models.SlugField(unique=True, max_length=255)
+	first_name = models.CharField(max_length=255)
+	last_name = models.CharField(max_length=255)
+	is_active = models.BooleanField(default=True)
+	is_admin = models.BooleanField(default=False)
+ 
+	# custom user manager
+	objects = CUserManager()
 
 	USERNAME_FIELD = "email"
 	REQUIRED_FIELDS = ["username", "first_name", "last_name"]
@@ -12,7 +46,7 @@ class CUser(AbstractUser):
 	prof_picture = models.ImageField(upload_to="users/prof_%Y", default="users/اصغر.jpg")
 	bio = models.TextField(blank=True, max_length=500)
 	phone_num = models.CharField(max_length=15, unique=True)
-	date_birth = models.DateField(blank=True, null=True)
+	birthdate = models.DateField(blank=True, null=True)
 	pub_email = models.EmailField(blank=True, max_length=255)
 	tel_id = models.CharField(blank=True, max_length=50)
 	github_id = models.CharField(blank=True, max_length=50)
@@ -21,6 +55,16 @@ class CUser(AbstractUser):
 
 	def __str__(self):
 		return self.username
+
+	def has_perm(self, perm, obj=None):
+		return self.is_admin
+
+	def has_module_perms(self, app_label):
+		return self.is_admin
+
+	@property
+	def is_staff(self):
+		return self.is_admin
 
 	class Meta:
 		ordering = ('id',)
