ee95ee17269edec2544e19553d097df588276205
asgharale <asgharale11@gmail.com>
2024-09-14 17:50:06 +0330

    styling templates

--- cafe/blog/views.py
+++ cafe/blog/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from blog.models import Article
 
 def home(request):
-    return render(request, "blog/main.html")
+    articles = Article.objects.all()
+    context = {
+        "articles" : articles
+    }
+    return render(request, "blog/main.html", context)
 
 def article_view(request, address):
     pass
